a76bee76849c5ff893cf7acbcc77d1d22716e3d0
DouglasKyrius <douglaskyrius@gmail.com>
2020-07-05 18:18:31 -0400

    final files

---
+++ anim.js
@@ -0,0 +1,31 @@
+anime({
+  targets: '#scorpion',
+  right: ['-1000', '0'],
+  duration: 1100,
+  easing: 'easeOutExpo'
+});
+
+anime({
+  targets: '#strip',
+  opacity: [0,1],
+  translateX: ['500px', 0],
+  duration: 1000,
+  delay:300,
+});
+
+anime({
+  targets: '#baraka',
+  opacity: [0,1],
+  translateX: ['500px', 0],
+  duration: 1000,
+  delay:500,
+});
+
+anime({
+  targets: '.title',
+  opacity: [0, 1],
+  translateX: ['500px', 0],
+  duration: 1000,
+  delay: 700,
+  easing: 'easeOutExpo' 
+});
